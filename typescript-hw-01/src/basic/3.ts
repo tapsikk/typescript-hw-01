9aa3703ff0f978ce474c50452d8b5f4adbd8a13d
tapsikk <tapsgame@gmail.com>
2024-05-19 18:45:23 +0300

    gen

---
+++ typescript-hw-01/src/basic/3.ts
@@ -0,0 +1,7 @@
+let notSure: string | number;
+
+type toggle = "enable" | "disable";
+let value: toggle;
+
+value = "enable"
+value = "disable"
---
+++ typescript-hw-01/src/basic/7.ts
@@ -0,0 +1,28 @@
+type page = {
+  title: string,
+  likes: number,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: string,
+  details?: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  };
+}
+
+const page1 = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2 = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
